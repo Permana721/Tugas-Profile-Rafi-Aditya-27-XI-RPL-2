ad3be981cae0b5330fd840d3a4be72c0dab0b58e
Permana721 <sdharma721@gmail.com>
2023-09-13 22:10:16 +0700

    first init github

---
+++ resources/views/cv.blade.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV - RAFI</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: whitesmoke;
+            margin: 0;
+            padding: 0;
+        }
+        .cv-container {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+            border-radius: 20px;
+        }
+        .cv-header {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .cv-header h1 {
+            margin: 5px;
+        }
+        .cv-header p {
+            font-size: 18px;
+            color: #555;
+        }
+        .cv-section {
+            margin-bottom: 20px;
+        }
+        .cv-section h2 {
+            margin: 5px 0;
+            color: #333;
+        }
+        .cv-item {
+            margin-bottom: 10px;
+        }
+        .cv-item strong {
+            display: inline-block;
+            width: 250px;
+        }
+        .cv-photo img{
+            max-width: 150px;
+
+            border: 3px solid black;
+            margin-right: 20px;
+            border-radius: 50%;
+        }
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+
+    <div class="cv-container">
+        <div class="cv-header">
+            <h1>Curriculum Vitae</h1>
+        </div>
+        <div class="cv-photo">
+            <div style="text-align: center">
+                <img src="https://th.bing.com/th/id/OIP.9OLanwqz0biqN8b9QijRqwHaHV?pid=ImgDet&rs=1" alt="Photo Profile">
+                <hr>
+            </div>
+        <div class="cv-section">
+            <h2>Personal Data</h2>
+            <div class="cv-item">
+                <strong>Name:</strong> Rafi Aditya Pasha
+            </div>
+            <div class="cv-item">
+                <strong>Gender:</strong> Laki-Laki
+            </div>
+            <div class="cv-item">
+                <strong>Address:</strong> Jl.Jatihandap No.23 RT 10, RW 9
+            </div>
+            <div class="cv-item">
+                <strong>Religion:</strong> Muslim
+            </div>
+            <div class="cv-item">
+                <strong>Place, Date of Birth:</strong> Bandung, 31 May 2007
+            </div>
+        </div>
+        <hr>
+        <div class="cv-section">
+            <h2>Education</h2>
+            <div class="cv-item">
+                <strong>SDN Galunggung Tasikmalaya</strong>
+                <p>Tahun: 2013-2019</p>
+                <strong>SMPN 49 Bandung</strong>
+                <p>Tahun: 2019-2022</p>
+                <strong>SMKN 2 Bandung</strong>
+                <p>Tahun: 2022-2025</p>
+            </div>
+        </div>
+        <hr>
+        <div class="cv-section">
+            <h2>Skill</h2>
+            <div class="cv-item">
+            <ul>
+                <li>Problem Solving</li>
+                <li>Make a Good Relationship</li>
+                <li>Good At Teamwork</li>
+            </ul>    
+            </div>
+        </div>
+        <hr>
+        <div class="cv-section">
+            <h2>Language
+
+            </h2>
+            <div class="cv-item">
+                <ul>
+                    <li>Indonesia</li>
+                    <li>English</li>
+                    <li>Sunda</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <div class="cv-section">
+            <h2>Contact</h2>
+            <div class="cv-item">
+                <strong>Email:</strong> rafiaditya391@gmail.com
+            </div>
+            <div class="cv-item">
+                <strong>Handphone:</strong> (+62) 857 3028 9283
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/pplg.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!--========== BOX ICONS ==========-->
+        <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+        <!--========== CSS ==========-->
+        <link rel="stylesheet" href="/css/styles.css">
+
+        <title>Profile PPLG</title>
+    </head>
+    <body>
+        <!--========== SCROLL TOP ==========-->
+        <a href="#" class="scrolltop" id="scroll-top">
+            <i class='bx bx-up-arrow-alt scrolltop__icon'></i>
+        </a>
+        
+        <!--========== HEADER ==========-->
+        <header class="l-header" id="header">
+            <nav class="nav bd-container">
+                <a href="" class="logo"><img src="/img/pplg.png" alt=""></a>
+                <a href="#" class="nav__logo"></a>
+
+                <div class="nav__menu" id="nav-menu">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a href="#home" class="nav__link active-link">Home</a></li>
+                        <li class="nav__item"><a href="#info" class="nav__link">Information</a></li>
+                        <li class="nav__item"><a href="#materi" class="nav__link">Materi</a></li>
+                        <li class="nav__item"><a href="/cv" class="nav__link">CV</a></li>
+                    </ul>
+                </div>
+
+                <div class="nav__toggle" id="nav-toggle">
+                    <i class='bx bx-grid-alt'></i>
+                </div>
+            </nav>
+        </header>
+
+        <main class="l-main">
+            <!--========== HOME ==========-->
+            <section class="home" id="home">
+                <div class="home__container bd-container bd-grid">
+                    <div class="home__img">
+                        <img src="/img/pplg.png" alt="">
+                    </div>
+
+                    <div class="home__data">
+                        <h1 class="home__title">Company Profile PPLG</h1>
+                        <p class="home__description">Website ini ditujukan untuk mengenal lebih lanjut mengenai jurusan PPLG dan juga memberikan informasi dan juga materi mengenai jurusan PPLG.</p>
+                    </div>   
+                </div>
+            </section>
+
+            <!--========== INFORMATION ==========-->
+            <section class="info section bd-container" id="info">
+                <div class="info__container">
+                    <div class="share__data">
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <h2 class="section-title">Apa Itu <br> PPLG?</h2>
+                        <span class="info__description">
+                            PPLG atau Pengembangan Perangkat Lunak dan Gim adalah sebuah jurusan di SMK (Sekolah Menengah Kejuruan).
+                            Jurusan ini mempelajari dan mendalami cara-cara mengembangkan perangkat lunak. Mulai dari pembuatan, pemeliharaan, manajemen kualitas,
+                            dan manajemen organisasi pengembangan perangkat lunak. Selain itu, jurusan PPLG juga berhubungan dengan software komputer seperti 
+                            pembuatan aplikasi, website, game dan semua yang berhubungan dengan pemrograman. Singkatnya, jurusan PPLG erat kaitannya dengan coding,
+                            desain, dan algoritma.
+                        </span>
+                    </div>
+
+                </div>
+            </section>
+
+            <!--========== MATERI ==========-->
+            <section class="container" id="materi">
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <h2 class="section-title">Materi</h2>
+
+                <div class="box-container">
+
+            <div class="box">
+                <img src="/img/icon-1.png" alt="">
+                <h3>HTML 5</h3>
+                <p>Hypertext Markup Language (HTML) adalah bahasa markah standar untuk dokumen yang dirancang untuk ditampilkan di peramban internet. Ini dapat dibantu oleh teknologi seperti Cascading Style Sheets (CSS) dan bahasa skrip lainnya seperti JavaScript, VBScript, dan PHP. Logo resmi dari versi terbaru, HTML5.</p>
+            </div>
+
+            <div class="box">
+                <img src="/img/icon-2.png" alt="">
+                <h3>CSS 3</h3>
+                <p>Cascading Style Sheets (CSS) merupakan bahasa pemrograman yang digunakan untuk menentukan bagaimana dokumen dan website akan disajikan. CSS dibuat oleh Word Wide Web Consortium (W3C) pada 1996.</p>
+            </div>
+
+            <div class="box">
+                <img src="/img/icon-3.png" alt="">
+                <h3>JavaScript</h3>
+                <p>JavaScript adalah bahasa pemrograman yang digunakan dalam pengembangan website agar lebih dinamis dan interaktif. Kalau sebelumnya kamu hanya mengenal HTML dan CSS, nah sekarang kamu jadi tahu bahwa JavaScript dapat meningkatkan fungsionalitas pada halaman web. Bahkan dengan JavaScript ini kamu bisa membuat aplikasi, tools, atau bahkan game pada web.</p>
+            </div>
+
+            </section>
+
+
+        <!--========== FOOTER ==========-->
+        <footer class="footer section">
+            <div class="footer__container bd-container bd-grid">
+                
+                <div class="footer__content">
+                    <h3 class="footer__title">Social</h3>
+                    <a href="#" class="footer__social"><i class='bx bxl-facebook-circle '></i></a>
+                    <a href="#" class="footer__social"><i class='bx bxl-twitter'></i></a>
+                    <a href="#" class="footer__social"><i class='bx bxl-instagram-alt'></i></a>
+                </div>
+            </div>
+
+        </footer>
+
+        <!--========== SCROLL REVEAL ==========-->
+        <script src="https://unpkg.com/scrollreveal"></script>
+
+        <!--========== MAIN JS ==========-->
+        <script src=""></script>
+    </body>
+</html>
